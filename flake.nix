0aa063deecacdb7797a46efadf62cb5fdf87a62f
Chris Dunphy <chris@megaparsec.ca>
2023-02-26 15:58:39 -0700

    Made the flake cross-platform

--- flake.nix
+++ flake.nix
@@ -1,16 +1,18 @@
 {
   description = "This is a program representing rational numbers in Python";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  outputs = { self, nixpkgs }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        inherit system;
-      };
-    in
-    {
-      devShell.${system} =
-        pkgs.mkShell
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell = pkgs.mkShell
           {
             # The packages we need for this project
             buildInputs = with pkgs;
@@ -18,9 +20,10 @@
                 rustc
                 cargo
                 clippy
+                libiconv
                 rust-analyzer
               ];
             RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
           };
-    };
+      });
 }
